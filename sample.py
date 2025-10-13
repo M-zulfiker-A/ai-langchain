315337cda22954fd43e531e6d9bab7868eb85a0e
Zulfiker <muhammedzulfikerindia@gmail.com>
2025-10-13 15:54:18 +0000

    decorator with logger

--- sample.py
+++ sample.py
@@ -1,8 +1,14 @@
-def logger(func):
-    print(func.__name__)
-    return func
+import logging
 
-@logger
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger("DecoratorLogger")
+def loggerFn(func : callable) -> callable:
+    def wrapper(*args, **kwargs):
+        logger.info(f"{func.__name__} was called with args: {args} and kwargs: {kwargs}")
+        return func(*args, **kwargs)
+    return wrapper
+
+@loggerFn
 def add(x, y):
     return x + y
 
